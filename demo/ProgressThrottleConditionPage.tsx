79e17d053ccf3c32dcc94b22427f885e15e3f907
songzhw <songzhw2012@gmail.com>
2021-06-22 18:19:12 -0400

    update examples

--- demo/ProgressThrottleConditionPage.tsx
+++ demo/ProgressThrottleConditionPage.tsx
@@ -5,7 +5,12 @@ const refresh = (prog: number) => {
   console.log(`szw progress = `, prog);
 };
 
-const condition = (prev: any, now: any) => (now[0] - prev[0] > 0.3);
+const condition = (prev: unknown, now: unknown) => {
+  if (Array.isArray(prev) && Array.isArray(now)) {
+    return now[0] - prev[0] > 0.3;
+  }
+  return true;
+};
 const reset = (arg: number[]) => arg[0] === 1;
 const throttledCallback = throttleCondition(refresh, [0], condition, reset);
 
